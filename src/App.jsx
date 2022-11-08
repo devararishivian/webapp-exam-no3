cd5e22368812be292ae2abec4c8cbd7d95318321
Devvara Rishivian <rishivian@gmail.com>
2022-11-08 23:10:16 +0700

    feat: ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,124 +8,57 @@ import {
 const pricingPlanData = [
   {
     id: 1,
-    icon: IconAnchor,
+    icon: () => <IconAnchor size={128}></IconAnchor>,
     title: "PIRATE PLAN",
     price: "950.000",
+    color: "text-red-500",
   },
   {
     id: 2,
-    icon: IconGitlab,
+    icon: () => <IconGitlab size={128}></IconGitlab>,
     title: "CATMAN PLAN",
     price: "1.590.000",
+    color: "text-sky-700",
   },
   {
     id: 3,
-    icon: IconAward,
+    icon: () => <IconAward size={128}></IconAward>,
     title: "CHAMP PLAN",
     price: "4.590.000",
+    color: "text-amber-500",
   },
 ];
 
-function AppCard({ icon, title, price }) {
+function AppCard({ icon: Icon, title, price, color }) {
   return (
-    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-      <h3 className="mb-4 text-2xl font-semibold">{title}</h3>
-      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-        Best option for personal use &amp; for your next project.
-      </p>
-      <div className="flex justify-center items-baseline my-8">
-        <span className="mr-2 text-5xl font-extrabold">$29</span>
-        <span className="text-gray-500 dark:text-gray-400">/month</span>
+    <div className="flex flex-col p-8 mx-auto w-80 text-center cursor-pointer text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <div className={`flex justify-center ${color} mb-12`}>
+        <Icon></Icon>
       </div>
-      <ul role="list" className="mb-8 space-y-4 text-left">
-        <li className="flex items-center space-x-3">
-          <svg
-            className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>Individual configuration</span>
+      <h3 className="text-2xl font-medium">{title}</h3>
+      <div className="relative flex py-3">
+        <div className="flex-grow border-t border-gray-400"></div>
+      </div>
+      <ul role="list" className="space-y-4 font-light my-5">
+        <li className="flex justify-center space-x-3">
+          <span>Lifetime update</span>
         </li>
-        <li className="flex items-center space-x-3">
-          <svg
-            className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>No setup, or hidden fees</span>
+        <li className="flex justify-center space-x-3">
+          <span>Daily rewards</span>
         </li>
-        <li className="flex items-center space-x-3">
-          <svg
-            className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>
-            Team size: <span className="font-semibold">1 developer</span>
-          </span>
-        </li>
-        <li className="flex items-center space-x-3">
-          <svg
-            className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>
-            Premium support: <span className="font-semibold">6 months</span>
-          </span>
-        </li>
-        <li className="flex items-center space-x-3">
-          <svg
-            className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            />
-          </svg>
-          <span>
-            Free updates: <span className="font-semibold">6 months</span>
-          </span>
+        <li className="flex justify-center space-x-3">
+          <span>Annually report</span>
         </li>
       </ul>
-      <a
-        href="#"
-        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
-      >
-        Get started
-      </a>
+      <div className="relative flex py-3">
+        <div className="flex-grow border-t border-gray-400"></div>
+      </div>
+      <div className="flex flex-col items-center mb-5">
+        <span className={`text-3xl font-medium ${color}`}>
+          IDR&nbsp;{price}
+        </span>
+        <span>per month</span>
+      </div>
     </div>
   );
 }
@@ -134,18 +67,29 @@ function App() {
   const [count, setCount] = useState(0);
 
   const pricingPlans = pricingPlanData.map((v) => (
-    <AppCard key={v.id} icon={v.icon} title={v.title} price={v.price} />
+    <AppCard
+      key={v.id}
+      icon={v.icon}
+      title={v.title}
+      price={v.price}
+      color={v.color}
+    />
   ));
 
   return (
     <div className="h-screen dark:bg-gray-900">
-      <section>
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-          <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-            {pricingPlans}
-          </div>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+          {pricingPlans}
         </div>
-      </section>
+      </div>
+      <div className="grid place-items-center mt-12">
+        <div className="flex flex-col items-center text-white">
+          <p className="text-xl">Qaisha Muhammada Devvara Rishivian</p>
+          <p className="text-lg">2301964590 | qaisha.rishivian@binus.ac.id</p>
+          <p>MGCA - Web App</p>
+        </div>
+      </div>
     </div>
   );
 }
